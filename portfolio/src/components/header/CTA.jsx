5ea5ee9526181e550fd837bc0e032f70cc025f87
D-Luffytaro <regina_@live.fr>
2024-06-17 01:50:39 +0200

    ✨ Create <CTA /> component

---
+++ portfolio/src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+export default function CTA() {
+  return (
+    <div className="cta">
+        <a className="btn">
+          Télécharger mon CV
+        </a>
+        <a className="btn btn-primary" href="">
+          À Propos de moi
+        </a>
+      </div>
+  )
+}
