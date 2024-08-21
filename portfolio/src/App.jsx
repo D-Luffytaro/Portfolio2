e6251ad772c77f20adf0ef0c7250b5989faf0009
D-Luffytaro <regina_@live.fr>
2024-08-21 22:17:32 +0200

    ✨ Add <Portfolio />

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import Contact from "./components/contact/Contact";
 import Experience from "./components/experience/Experience";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Portfolio from "./components/portfolio/Portfolio";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <About />
       <Experience />
+      <Portfolio />
       <Contact />
       <Footer />
     </>
---
+++ portfolio/src/components/portfolio/data.jsx
@@ -0,0 +1,25 @@
+import IMG1 from "../../assets/images/cd.png";
+import IMG2 from "../../assets/images/porfolio.png";
+import IMG3 from "../../assets/images/mfp.png";
+
+export const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Crazee Burger",
+    link: "https://github.com/D-Luffytaro/crazee-burger",
+    demo: "https://crazy-bg-d-luffytaro-d-luffytaros-projects.vercel.app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Portfolio",
+    link: "https://github.com/D-Luffytaro/Portfolio2",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Maquette Web",
+    link: "https://www.behance.net/gallery/201581079/Maquette-MFP",
+  },
+];
